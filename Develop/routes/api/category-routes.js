6e07c89f5bd2e7d45b1837aa0252851728ed612f
rtm713 <rtm713@gmail.com>
2023-08-13 16:02:25 -0500

    updated Readme

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -58,7 +58,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', (req, res) => {
   try {
     Category.destroy({ where: {id: req.params.id}});
-    res.status(200);
+    res.status(200).json({message: "category deleted"});
   } catch (err) {
     res.status(400).json(err);
   }
